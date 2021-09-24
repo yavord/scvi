6044df1dd48700cab02e37076e553cd1077ccd19
yav-d <yavor.d0@gmail.com>
2021-09-24 10:59:18 -0500

    init

---
+++ intro.py
@@ -0,0 +1,26 @@
+import sys
+import scvi
+import scanpy as sc
+
+
+sc.set_figure_params(figsize=(4, 4))
+
+# load data/check if its there
+adata = scvi.data.heart_cell_atlas_subsampled()
+
+# preprocess lowly expressed genes
+sc.pp.filter_genes(adata, min_counts=3)
+
+adata.layers["counts"] = adata.X.copy() # preserve counts
+sc.pp.normalize_total(adata, target_sum=1e4)
+sc.pp.log1p(adata)
+adata.raw = adata # freeze the state in `.raw`
+
+sc.pp.highly_variable_genes(
+    adata,
+    n_top_genes=1200,
+    subset=True,
+    layer="counts",
+    flavor="seurat_v3",
+    batch_key="cell_source"
+)
