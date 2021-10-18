9b5bc328df09c30189482688a3d6ba2bd0de86db
yav-d <yavor.d0@gmail.com>
2021-10-18 14:28:34 +0300

    dendrograms

--- intro/modelLoad.py
+++ intro/modelLoad.py
@@ -22,49 +22,84 @@ adata.layers["scvi_normalized"] = model.get_normalized_expression(library_size=1
 
 ### UMAP without batch correction
 # run PCA -> UMAP plots
-# sc.tl.pca(adata, svd_solver='arpack')
-# sc.pp.neighbors(adata, n_pcs=30, n_neighbors=20)
-# sc.tl.umap(adata, min_dist=0.3)
+sc.tl.pca(adata, svd_solver='arpack')
+sc.pp.neighbors(adata, n_pcs=30, n_neighbors=20)
+sc.tl.umap(adata, min_dist=0.3)
 
-# sc.pl.umap(
-#     adata,
-#     color=["cell_type"],
-#     frameon=False,
-# )
-# sc.pl.umap(
-#     adata,
-#     color=["donor", "cell_source"],
-#     ncols=2,
-#     frameon=False,
-# )
+sc.pl.umap(
+    adata,
+    color=["cell_type"],
+    frameon=False,
+)
+sc.pl.umap(
+    adata,
+    color=["donor", "cell_source"],
+    ncols=2,
+    frameon=False,
+)
 
 ### UMAP with batch correction
-# sc.pp.neighbors(adata, use_rep="X_scVI") # use latent space
-# sc.tl.umap(adata, min_dist=0.3)
+sc.pp.neighbors(adata, use_rep="X_scVI") # use latent space
+sc.tl.umap(adata, min_dist=0.3)
 
-# sc.pl.umap(
-#     adata,
-#     color=["cell_type"],
-#     frameon=False,
-# )
-# sc.pl.umap(
-#     adata,
-#     color=["donor", "cell_source"],
-#     ncols=2,
-#     frameon=False,
-# )
+sc.pl.umap(
+    adata,
+    color=["cell_type"],
+    frameon=False,
+)
+sc.pl.umap(
+    adata,
+    color=["donor", "cell_source"],
+    ncols=2,
+    frameon=False,
+)
 
 # 1v1 DE cell types, Myeloid vs Fibroblast
-# de_df = model.differential_expression(
-#     groupby="cell_type",
-#     group1="Myeloid",
-#     group2="Fibroblast"
-# )
-# DataFrame.to_csv(self=de_df, path_or_buf="output/df/1v1DE.csv")
+de_df = model.differential_expression(
+    groupby="cell_type",
+    group1="Myeloid",
+    group2="Fibroblast"
+)
+DataFrame.to_csv(self=de_df, path_or_buf="output/df/1v1DE.csv")
 
 # 1vAll DE, all cell types 
-# de_df = model.differential_expression(
-#     groupby="cell_type"
-# )
-# DataFrame.to_csv(self=de_df, path_or_buf="output/df/1vAllDE.csv")
+de_df = model.differential_expression(
+    groupby="cell_type"
+)
+DataFrame.to_csv(self=de_df, path_or_buf="output/df/1vAllDE.csv")
 
+markers = {}
+cats = adata.obs.cell_type.cat.categories
+for i, c in enumerate(cats):
+    cid = "{} vs Rest".format(c)
+    cell_type_df = de_df.loc[de_df.comparison == cid]
+
+    cell_type_df = cell_type_df[cell_type_df.lfc_mean > 0]
+
+    cell_type_df = cell_type_df[cell_type_df["bayes_factor"] > 3]
+    cell_type_df = cell_type_df[cell_type_df["non_zeros_proportion1"] > 0.1]
+
+    markers[c] = cell_type_df.index.tolist()[:3]
+
+sc.tl.dendrogram(adata, groupby="cell_type", use_rep="X_scVI")
+
+sc.pl.dotplot(
+    adata,
+    markers,
+    groupby='cell_type',
+    dendrogram=True,
+    color_map="Blues",
+    swap_axes=True,
+    use_raw=True,
+    standard_scale="var",
+)
+
+sc.pl.heatmap(
+    adata,
+    markers,
+    groupby='cell_type',
+    layer="scvi_normalized",
+    standard_scale="var",
+    dendrogram=True,
+    figsize=(8, 12)
+)
